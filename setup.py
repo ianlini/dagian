43938911c66d17e54dec0222cb970b50885b2468
Ian Lin <ian.yian.lin@gmail.com>
2018-09-29 17:10:17 +0800

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('README.rst') as fp:
 
 setup(
     name='dagian',
-    version="0.0.1",
+    version="0.0.2",
     description=description,
     long_description=long_description,
     author='Ian Lin',
